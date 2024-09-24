4233d521a17c064d492a32671835b0d876224af6
Shannnon Evora Lopes Rodrigues <shannonelr9208@icloud.com>
2024-09-24 11:33:16 +0200

    use select,where and deleted

--- bd/netflix.sql
+++ bd/netflix.sql
@@ -28,15 +28,38 @@ VALUES
 ('Pulp Fiction', 'Crimen.', 'https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg', 'Top 10', '1994'),
 ('La vita è bella', 'Comedia', 'https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg', 'Top 10', '1996'),
 ('Forrest Gump', 'Comedia', 'https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg', 'Top 10', '1994');
-select * from movies;
+
 INSERT INTO Users (user, password, name, email, plan_details)
 VALUES
 ('laura_dev', 'laura', 'Laura', 'laura@gmail.com', 'Standard'),
 ('maria_dev', 'maria', 'Maria', 'maria@gmail.com', 'Standard'),
 ('ester_dev', 'ester', 'Ester', 'ester@gmail.com', 'Standard');
-select * from Users;
+
 INSERT INTO Actors (name, lastname, country, birthday)
 VALUES
 ('Tom', 'Hanks', 'Estados Unidos', '1956-07-09'),
 ('Roberto', 'Benigni', 'Italia', '1952-10-27'),
 ('John', 'Travolta', 'Estados Unidos', '1954-02-18');
+-- Sobre movies
+SELECT * FROM movies;
+SELECT title, genre FROM movies WHERE year > 1990;
+SELECT * FROM movies WHERE category = 'TOp 10';
+UPDATE movies SET year = 1997 WHERE idMovies = 2;
+-- Sobre actores
+SELECT * FROM Actors;
+SELECT * FROM Actors WHERE birthday BETWEEN '1950-01-01' AND '1960-12-31';
+SELECT name, lastname FROM Actors WHERE country = 'Estados Unidos';
+-- Sobre los usuarios
+
+
+SELECT * FROM Users;
+SELECT * FROM Users WHERE plan_details = 'Standard';
+
+
+SELECT * FROM Users WHERE name LIKE 'M%';
+DELETE FROM Users WHERE name LIKE 'M%';
+
+
+
+
+
