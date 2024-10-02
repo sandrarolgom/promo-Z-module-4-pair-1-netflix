07ed106dc61aa2d849e57708028bfd136fe80f4a
sandrarolgom <sandraroldan210900@gmail.com>
2024-10-02 11:27:42 +0200

    connect with server and add movies

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ async function connectDB(){
   const connection = await mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "1234",
+    password: "Vertmysql21&",
     database: "netflix"
 });
   await connection.connect();
@@ -31,33 +31,9 @@ server.get("/movies", async(req, res)=>{
   const connectionDB= await connectDB();
   const sqlQuery="select * from movies";
   const [resultMovies]= await connectionDB.query(sqlQuery);
-  console.log(resultMovies);
- 
-  /*const fakeMovies = [
-    {
-      id: 1,
-      title: "Wonder Woman",
-      genre: "Action",
-      image:
-        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-      category: "Superhero",
-      year: 2017,
-      director: "Patty Jenkins",
-    },
-    {
-      id: 2,
-      title: "Inception",
-      genre: "Science Fiction",
-      image:
-        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-      category: "Thriller",
-      year: 2010,
-      director: "Christopher Nolan",
-    },
-  ];*/
-  
+  //console.log(resultMovies);
   res.json({
     success: true,
-    movies:  fakeMovies
+    movies:  resultMovies
   })
 })
